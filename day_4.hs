1e9ba30af14e5afb36d95f67d886482400ef1499
Holden Grissett <holdeng96@gmail.com>
2023-12-30 17:39:12 -0800

    add day 4 part 2
    Signed-off-by: Holden Grissett <holdeng96@gmail.com>

--- day_4.hs
+++ day_4.hs
@@ -2,8 +2,14 @@
 
 import Data.List
 import Data.List.Split (splitOn)
+import Data.Maybe (fromJust)
 --import qualified Data.Set as Set
 import Debug.Trace
+
+
+import qualified Data.HashTable.IO as H
+import qualified Control.Monad.Trans.Accum as H
+type HashTable k v = H.BasicHashTable k v
 
 
 get_cards :: String -> [([Int], [Int])]
@@ -34,6 +40,50 @@ points_sum input =
     in foldl (\acc card -> acc + card_points card) 0 cards
 
 
+type Card = (Int, ([Int], [Int]))
+get_cards_ht :: String -> IO (HashTable Int Card)
+
+get_cards_ht input = do
+        ht <- H.new
+        let cards = lines input
+        let cards' = map (\card -> last $ splitOn ":" card) cards
+        let cards'' = map (\card -> (map read $ words $ head $ splitOn "|" card,
+                                 map read $ words $ last $ splitOn "|" card)) cards'
+        mapM_ (\(i, card) -> H.insert ht i (1, card)) (zip [1..] cards'')
+        return ht
+
+matches :: Card -> Int
+matches card =
+    sum [1 | x <- (fst $ snd card), y <- (snd $ snd card), x == y]
+
+getRange :: Int -> Int -> [a] -> [a]
+getRange start end = take (end - start + 1)
+
+total_cards :: HashTable Int Card -> IO Int
+total_cards ht = do
+    cards_list <- H.toList ht
+    let updateCounts = mapM_ (updateCard ht) $ reverse cards_list
+    updateCounts
+    final_cards_list <- H.toList ht
+    print $ reverse final_cards_list
+    return $ sum $ map (fst . snd) final_cards_list
+
+-- Helper function to update the counts of subsequent cards
+updateCard :: HashTable Int Card -> (Int, Card) -> IO ()
+updateCard ht (i, _) = do
+    cardM <- H.lookup ht i
+    case cardM of
+        Nothing -> return ()
+        Just card -> do
+            let copies = fst card
+            let matchCount = matches card
+            -- Update subsequent cards based on the number of matches
+            if matchCount == 0 then return ()
+            else do
+                print $ "Updating at card " ++ show i ++ " with " ++ show copies ++ " copies"
+                mapM_ (\j -> H.mutate ht j (\(Just (count, card2)) ->
+                            (Just (count + copies, card2), ()))) [i+1 .. i+matchCount]
+
 main = do
     let test_input = "Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53\n\
                      \Card 2: 13 32 20 16 61 | 61 30 68 82 17 32 24 19\n\
@@ -42,5 +92,7 @@ main = do
                      \Card 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36\n\
                      \Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11"
     content <- readFile "input_day_4.txt"
-    --print $ get_cards test_input
-    print $ points_sum content
+    ht <- get_cards_ht content
+    total <- total_cards ht
+    print total
+    --print $ points_sum content
